667e9e45891f9fb8a580923f92160728dedff293
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-09-07 18:32:05 -0400

    Rename Todos to Todo class.

---
+++ app/src/main/java/com/raco/starbuzz/Todo.java
@@ -0,0 +1,12 @@
+package com.raco.starbuzz;
+
+public class Todo {
+    //TODO: Add the Food, Store, Books, Mugs drawable resources
+    //TODO: Create Food, Store, Book, Mug classes
+    //TODO: Create FoodCategoryActivity and StoreCategoryActivity, BookCategory,
+    //      MugCategory and layouts
+    //TODO: Modify top level activity logic to detect clicks in all positions and
+    //      call corresponding categories
+    //TODO: Create FoodActivity, StoreActivity, BookActivity, MugActivity classes and corresponding layouts
+    //TODO: Test
+}
