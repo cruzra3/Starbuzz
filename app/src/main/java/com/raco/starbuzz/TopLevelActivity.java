f6bc352f115bf65fe0f8debcdacb5f416bfb957d
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-09-07 12:54:36 -0400

    Create drink category activity and corresponding layout.  Add activity to manifest.  Add listener to top level activity.

--- app/src/main/java/com/raco/starbuzz/TopLevelActivity.java
+++ app/src/main/java/com/raco/starbuzz/TopLevelActivity.java
@@ -2,7 +2,11 @@ package com.raco.starbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class TopLevelActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class TopLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+        //Create an OnItemClickListener
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> listView,
+                                            View itemView,
+                                            int position,
+                                            long id) {
+                        if (position == 0) {
+                            Intent intent = new Intent(TopLevelActivity.this,
+                                    DrinkCategoryActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                };
+        //Add the listener to the list view
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
