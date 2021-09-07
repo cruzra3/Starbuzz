0e959d3cb373bd3490a0e65121175ac23d8b8254
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-09-07 14:01:06 -0400

    Add logic to drink category activity, add a listener to detect clicks on the items in the list of drinks.  When a click is detected then send the id of the item in an intent to the next activity: drink activity.

--- app/src/main/java/com/raco/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/raco/starbuzz/DrinkCategoryActivity.java
@@ -2,7 +2,10 @@ package com.raco.starbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -18,5 +21,24 @@ public class DrinkCategoryActivity extends AppCompatActivity {
                 Drink.drinks);
         ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
         listDrinks.setAdapter(listAdapter);
+
+        //Create the listener
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> listDrinks,
+                                            View itemView,
+                                            int position,
+                                            long id) {
+                        //Pass the drink the user clicks on to DrinkActivity
+                        Intent intent = new Intent(DrinkCategoryActivity.this,
+                                DrinkActivity.class);
+                        intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int) id);
+                        startActivity(intent);
+                    }
+                };
+
+        //Assign the listener to the list view
+        listDrinks.setOnItemClickListener(itemClickListener);
     }
 }
