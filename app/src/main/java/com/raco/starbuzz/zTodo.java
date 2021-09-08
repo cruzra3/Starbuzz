2be373c0fe631946b62ae3f74d288eb470f3f75e
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-09-08 13:52:14 -0400

    Tested Drinks, Food, and Stores logic.

--- app/src/main/java/com/raco/starbuzz/zTodo.java
+++ app/src/main/java/com/raco/starbuzz/zTodo.java
@@ -1,13 +1,16 @@
 package com.raco.starbuzz;
 
 public class zTodo {
-    //xTODO: Add the Food, Store, Books, Mugs drawable resources
-    //xTODO: Create Food, Store, Book, Mug classes
+    //xTODO: Add the Food, Store, Books, Mugs drawable resources.
+    //xTODO: Create Food, Store, Book, Mug classes.
     //xTODO: Create FoodCategoryActivity and StoreCategoryActivity, BookCategory,
-    //      MugCategory and layouts
+    //      MugCategory and layouts.
     //xTODO: Modify top level activity logic to detect clicks in all positions and
-    //      call corresponding categories
-    //xTODO: Test from initial screen to category screen
-    //TODO: Create FoodActivity, StoreActivity, BookActivity, MugActivity classes and corresponding layouts
-    //TODO: Test from category to edit detail
+    //      call corresponding categories.
+    //xTODO: Test from initial screen to category screen.
+    //xTODO: Create FoodActivity, StoreActivity, BookActivity, MugActivity classes and corresponding layouts.
+    //xTODO: Test from category to edit detail.
+    //TODO: Added succesully Food and Store logic, still to add Books and Mugs logic
+    //TODO: When adding Books and Mugs delete the x's from above lines for testing purposes
+    //TODO: The drawables for Mugs and Books are in drawable folder.
 }
