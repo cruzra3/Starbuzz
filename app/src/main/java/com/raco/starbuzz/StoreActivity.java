25c823e7d40c30e544e31bbb3ed6e6284215e23b
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-09-08 13:40:54 -0400

    Add logic to store activity to display store info.

--- app/src/main/java/com/raco/starbuzz/StoreActivity.java
+++ app/src/main/java/com/raco/starbuzz/StoreActivity.java
@@ -3,6 +3,8 @@ package com.raco.starbuzz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class StoreActivity extends AppCompatActivity {
     public static final String EXTRA_STOREID = "storeid";
@@ -11,5 +13,23 @@ public class StoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
+
+        //Get the store from the intent
+        int storeId = (Integer)getIntent().getExtras().get(EXTRA_STOREID);
+        Store store = Store.stores[storeId];
+
+        //Populate the store name
+        TextView name = (TextView)findViewById(R.id.name);
+        name.setText(store.getName());
+
+        //Populate the store description
+        TextView description = (TextView)findViewById(R.id.description);
+        description.setText(store.getDescription());
+
+        //Populate the store image
+        ImageView photo = (ImageView)findViewById(R.id.photo);
+        photo.setImageResource(store.getImageResourceId());
+        photo.setContentDescription(store.getName());
+
     }
 }
--- app/src/main/java/com/raco/starbuzz/zTodo.java
+++ app/src/main/java/com/raco/starbuzz/zTodo.java
@@ -3,11 +3,11 @@ package com.raco.starbuzz;
 public class zTodo {
     //xTODO: Add the Food, Store, Books, Mugs drawable resources
     //xTODO: Create Food, Store, Book, Mug classes
-    //TODO: Create FoodCategoryActivity and StoreCategoryActivity, BookCategory,
+    //xTODO: Create FoodCategoryActivity and StoreCategoryActivity, BookCategory,
     //      MugCategory and layouts
-    //TODO: Modify top level activity logic to detect clicks in all positions and
+    //xTODO: Modify top level activity logic to detect clicks in all positions and
     //      call corresponding categories
-    //TODO: Test from initial screen to category screen
+    //xTODO: Test from initial screen to category screen
     //TODO: Create FoodActivity, StoreActivity, BookActivity, MugActivity classes and corresponding layouts
     //TODO: Test from category to edit detail
 }
